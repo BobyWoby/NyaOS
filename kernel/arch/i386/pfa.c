c941f60f16777a06691d8baefb1bfc6b5560357a
Ken Zheng <kenzheng@umich.edu>
2026-06-22 22:29:56 -0700

    sync

---
+++ kernel/arch/i386/pfa.c
@@ -0,0 +1,24 @@
+#include <kernel/page.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define FREE 0x00
+#define USED 0x01
+#define ERROR 0x01
+#define NUM_PAGES 20
+
+typedef uintptr_t pageframe_t;
+
+uint32_t frame_map[NUM_PAGES];
+
+
+static pageframe_t kalloc_frame_int(){
+    uint32_t i = 0;
+    while(frame_map[i] != FREE){
+        if(++i == NUM_PAGES){
+            return (ERROR);
+        }
+
+    }
+}
+
---
+++ kernel/include/kernel/page.h
@@ -0,0 +1,18 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct page{
+
+} page_t;
+typedef struct page_table{
+
+} page_table_t;
+
+typedef struct page_directory{
+
+}page_directory_t;
+
+#endif
