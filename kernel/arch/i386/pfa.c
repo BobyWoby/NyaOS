4ef1e86f54d7e32da43378ee166129b397e8e9f6
Ken Zheng <kenzheng@umich.edu>
2026-06-23 23:26:38 -0700

    started pfa

--- kernel/arch/i386/pfa.c
+++ kernel/arch/i386/pfa.c
@@ -2,24 +2,22 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define FREE 0x00
-#define USED 0x01
-#define ERROR 0x01
-#define NUM_PAGES 20
+#define PAGE_SIZE 0x1000
+static uint32_t npages;
+static uint32_t* bitmap;
+
+extern char endkernel; // only care abt the address of this
+
+static inline void bm_set(uint32_t i) { bitmap[i >> 5] |= (1u << (i & 0b11111)); }
+static inline void bm_clear(uint32_t i) { bitmap[i >> 5] &= ~(1u << (i & 0b11111)); }
+static inline int bm_test(uint32_t i) { return bitmap[i >> 5] & (1u << (i & 0b11111)); }
 
 typedef uintptr_t pageframe_t;
 
-uint32_t frame_map[NUM_PAGES];
-
-
-static pageframe_t kalloc_frame_int(){
-    uint32_t i = 0;
-    while(frame_map[i] != FREE){
-        if(++i == NUM_PAGES){
-            return (ERROR);
-        }
-    }
-    frame_map[i] = USED;
-    // return (startframe + (i * 0x1000));
+static void pfa_init(uint32_t mem_high) {
+    npages = mem_high / PAGE_SIZE;
+    uintptr_t addr = ((uintptr_t)&endkernel + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
+    bitmap = (uint32_t *)addr;
 }
 
+static pageframe_t kalloc_frame_int() {}
