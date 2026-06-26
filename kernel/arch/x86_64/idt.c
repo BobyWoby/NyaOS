fea715743b202322fbe0447f90b13524764022fc
kzheng-stripe <kzheng@stripe.com>
2026-06-26 15:39:00 -0700

    removed paging for now, and started on interrupts

--- kernel/arch/x86_64/idt.c
+++ kernel/arch/x86_64/idt.c
@@ -3,14 +3,32 @@
 #include <stdint.h>
 
 typedef struct {
-  uint16_t isr_low;
-  uint16_t kernel_cs; // The GDT segment selector that the CPU will load into CS
-                      // before calling the ISR
-  uint8_t ist; // The IST in the TSS that the CPU will load into RSP; set to
-               // zero for now
-  uint8_t attributes; // Type and attributes; see the IDT page
-  uint16_t
-      isr_mid; // The higher 16 bits of the lower 32 bits of the ISR's address
-  uint32_t isr_high; // The higher 32 bits of the ISR's address
-  uint32_t reserved; // Set to zero
+  uint16_t offset_lo;
+  uint16_t ss; // segment selector
+  uint8_t ist; // only bottom 3 bits used
+
+  // Flag bits
+  // 0-3: gate type
+  //    0xe for 64-bit interrupt gate
+  //    0xf for 64-bit trap gate
+  // 4: 0
+  // 5-6: DPL (privilege level)
+  // 7: Present bit
+  //
+  // Common flag settings
+  // 64-bit Interrupt Gate: 0x8E (p=1, dpl=0b00, type=0b1110 =>
+  // flags=0b1000_1110=0x8E)
+  //
+  // 64-bit Trap Gate: 0x8F (p=1, dpl=0b00, type=0b1111 =>
+  // flags=1000_1111b=0x8F)
+  uint8_t flags;
+  uint16_t offset_mid;
+  uint32_t offset_hi;
+  uint32_t reserved;
 } __attribute__((packed)) idt_entry_t;
+
+typedef uint64_t idt_ptr_t;
+
+idt_ptr_t fill_idt_ptr(uint64_t offset, uint16_t size) {
+  return (offset << 16) | (size);
+}
